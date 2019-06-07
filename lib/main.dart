cb547b64831c64a65bbc29f17d2d319b8674138f
Zfinix <ogbondachiziaruhoma@gmail.com>
2019-06-07 21:20:01 +0100

    new project

--- lib/main.dart
+++ lib/main.dart
@@ -27,18 +27,33 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Image image = Image.asset('assets/images/food1.jpg');
+  bool isLiked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
+      appBar: buildAppBar(),
+      body: Center(
+        child: buildUI(),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  buildAppBar() => AppBar(
+        title: Text(
+          'Foods',
+          style: TextStyle(
+              color: Colors.orange, fontWeight: FontWeight.w600, fontSize: 25),
+        ),
         elevation: 0,
         actions: <Widget>[
           IconButton(
-            onPressed: () {},
+            onPressed: () => setState(() {
+                  isLiked ? isLiked = false : isLiked = true;
+                }),
             icon: Icon(
-              Icons.favorite_border,
+              isLiked ? Icons.favorite : Icons.favorite_border,
               color: Colors.orange,
             ),
           ),
@@ -50,109 +65,103 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           )
         ],
-      ),
-      body: Center(
-        child: buildUI(),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+      );
 
-  buildSlider() {
-    return Container(
-        width: screenWidth(context),
-        height: 300,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            new FoodCard(
-              title: 'Barbeque',
-              subtitle: 'French, Meat',
-              image: 'food2',
-              price: '5,500',
-            ),
-            new FoodCard(
-              title: 'Bread Bun',
-              subtitle: 'American, Bread',
-              image: 'food1',
-              price: '2,000',
-            ),
-              new FoodCard(
-              title: 'Cheese Pizza',
-              subtitle: 'Italian, Pizza',
-              image: 'food3', 
-              price: '2,000',
-            ),
-          ],
-        ));
-  }
-
-   buildUI() => Column(
+  buildUI() => Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          customYMargin(20),
           Flexible(
             flex: 3,
             child: buildSlider(),
           ),
-          Flexible(
-            flex: 2,
-            child: Container(
-                decoration: new BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    new BoxShadow(
-                        color: Colors.grey[100],
-                        blurRadius: 4.0,
-                        spreadRadius: 3.5,
-                        offset: Offset(0.0, 2)),
-                  ],
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                //   height: 200,
-                padding: const EdgeInsets.all(15.0),
-                margin: const EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'About',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
-                    ),
-                    customYMargin(10),
-                    Divider(),
-                    Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            height: 1.5, fontWeight: FontWeight.w300)),
-                  ],
-                )),
-          ),
+          buildAbout(),
           customYMargin(20),
           Flexible(
             flex: 1,
             child: Container(
-                width: (screenWidth(context) * 0.9), child: buildButton()),
+                width: (screenWidth(context) * 0.91), child: buildButton()),
           ),
         ],
       );
 
-   imageBuilder() => AnimatedContainer(
-      child: image,
-      curve: Curves.linear,
-      duration: Duration(seconds: 5),
-    );
+  buildSlider() => Container(
+      width: screenWidth(context),
+      height: 300,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          new FoodCard(
+            title: 'Barbeque',
+            subtitle: 'French, Meat',
+            image: 'food2',
+            price: '5,500',
+          ),
+          new FoodCard(
+            title: 'Bread Bun',
+            subtitle: 'American, Bread',
+            image: 'food1',
+            price: '2,000',
+          ),
+          new FoodCard(
+            title: 'Cheese Pizza',
+            subtitle: 'Italian, Pizza',
+            image: 'food3',
+            price: '7,200',
+          ),
+        ],
+      ));
+
+  buildAbout() => Flexible(
+        flex: 2,
+        child: Container(
+            decoration: new BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                new BoxShadow(
+                    color: Colors.grey[100],
+                    blurRadius: 4.0,
+                    spreadRadius: 3.5,
+                    offset: Offset(0.0, 2)),
+              ],
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            //   height: 200,
+            padding: const EdgeInsets.all(15.0),
+            margin: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'About',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
+                ),
+                customYMargin(10),
+                Divider(),
+                Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(height: 1.5, fontWeight: FontWeight.w300)),
+              ],
+            )),
+      );
+
+  imageBuilder() => AnimatedContainer(
+        child: image,
+        curve: Curves.linear,
+        duration: Duration(seconds: 5),
+      );
 
   buildButton() => CupertinoButton(
-      color: Colors.orange,
-      pressedOpacity: 0.6,
-      child: Text(
-        'View Ingredients',
-        style: TextStyle(color: Colors.white),
-      ),
-      onPressed: () {},
-    );
+        color: Colors.orange,
+        pressedOpacity: 0.6,
+        child: Text(
+          'View Ingredients',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+      );
 }
 
 class FoodCard extends StatelessWidget {
@@ -193,38 +202,38 @@ class FoodCard extends StatelessWidget {
   }
 
   buildListTile() => ListTile(
-      contentPadding: EdgeInsets.only(left: 33, top: 13),
-      title: Text(
-        '$title',
-        style: TextStyle(fontSize: 24),
-      ),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          customYMargin(8),
-          Text(
-            '$subtitle',
-            style: TextStyle(fontSize: 13, fontWeight: FontWeight.w300),
+        contentPadding: EdgeInsets.only(left: 33, top: 13),
+        title: Text(
+          '$title',
+          style: TextStyle(fontSize: 24),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            customYMargin(8),
+            Text(
+              '$subtitle',
+              style: TextStyle(fontSize: 13, fontWeight: FontWeight.w300),
+            ),
+            Divider(),
+          ],
+        ),
+        trailing: Container(
+          margin: EdgeInsets.only(
+            right: 18,
           ),
-          Divider(),
-        ],
-      ),
-      trailing: Container(
-        margin: EdgeInsets.only(
-          right: 18,
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Text(
+            'N $price',
+            style: TextStyle(
+                color: Colors.white, fontSize: 13, fontWeight: FontWeight.w300),
+          ),
         ),
-        padding: EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          color: Colors.orange,
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: Text(
-          'N $price',
-          style: TextStyle(
-              color: Colors.white, fontSize: 13, fontWeight: FontWeight.w300),
-        ),
-      ),
-    );
+      );
 }
 
 customYMargin(double y) => SizedBox(height: y);
